9d84bcde5920d3e9a00b720935ceb3e4b89e961e
ArchILLtect <nickhansonsr@gmail.com>
2025-08-05 05:59:38 -0500

    Update: add username display to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,25 +3,42 @@ import { Box,
   HStack,
   Link,
   Heading,
-  Button,
-  useColorModeValue,
-  useDisclosure
+  Text,
+  useColorModeValue
 } from '@chakra-ui/react';
 import { NavLink as RouterLink } from 'react-router-dom';
 import { useBudgetStore } from '../state/budgetStore';
-import AccountSyncModal from './accountSyncModal';
 
 // TODO: Add for mobile: import { HamburgerIcon } from '@chakra-ui/icons';
 
 export default function NavBar() {
 
-  const currentPage = useBudgetStore((s) => s.currentPage);
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const user = useBudgetStore((s) => s.user);
+  const demoUser = useBudgetStore((s) => s.isDemoUser);
 
   return (
     <Box position="sticky" top="0" zIndex="1000" bg={useColorModeValue('teal.500', 'teal.700')} px={4} py={3} shadow="md">
       <Flex h={8} alignItems={'center'} justifyContent={'space-between'}>
         <Heading size="md" color="white">Budgeteer</Heading>
+
+        {user && !demoUser ? (
+          <Text color="white" fontSize="sm"
+            fontWeight="bold" textTransform="uppercase"
+            letterSpacing="wider"
+            textAlign="center"
+            >
+              {user.username}
+          </Text>
+        ) : demoUser ? (
+          <Text color="white" fontSize="sm"
+            fontWeight="bold" textTransform="uppercase"
+            letterSpacing="wider"
+            textAlign="center">
+              Demo User
+          </Text>
+        ) : (
+          ""
+        )}
 
         <HStack as="nav" spacing={6}>
           <Link

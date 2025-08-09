9a7c23cd1908836e2dec79eefbd4f01c7fcda628
ArchILLtect <nickhansonsr@gmail.com>
2025-08-09 13:20:08 -0500

    Add: a reusable progress bar in modal

---
+++ src/components/ProgressModal.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Progress,
+  Text,
+  VStack,
+  Spinner,
+} from '@chakra-ui/react';
+import { useBudgetStore } from '../state/budgetStore';
+
+export default function ProgressModal() {
+  const isProgressOpen = useBudgetStore(s => s.isProgressOpen);
+  const progressCount = useBudgetStore(s => s.progressCount);
+  const progressTotal = useBudgetStore(s => s.progressTotal);
+
+  const percent = progressTotal > 0 ? (progressCount / progressTotal) * 100 : 0;
+
+  return (
+    <Modal isOpen={isProgressOpen} onClose={() => {}} isCentered size="md">
+      <ModalOverlay />
+      <ModalContent p={4}>
+        <ModalHeader>Applying Transactions</ModalHeader>
+        <ModalBody>
+          <VStack spacing={4}>
+            <Spinner size="lg" />
+            <Text>
+              {progressCount} / {progressTotal} processed
+            </Text>
+            <Progress
+              value={percent}
+              size="lg"
+              colorScheme="blue"
+              hasStripe
+              isAnimated
+              w="100%"
+              borderRadius="md"
+            />
+          </VStack>
+        </ModalBody>
+        <ModalFooter>
+          {percent >= 100 && (
+            <Text color="green.500" fontWeight="bold">
+              Done!
+            </Text>
+          )}
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}

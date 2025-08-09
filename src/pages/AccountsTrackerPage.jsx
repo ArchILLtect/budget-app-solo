59a91a1e33fb9cc24f0a4eb23ca3b6772a445825
ArchILLtect <nickhansonsr@gmail.com>
2025-08-09 13:49:13 -0500

    Update: better size styling

--- src/pages/AccountsTrackerPage.jsx
+++ src/pages/AccountsTrackerPage.jsx
@@ -1,18 +1,13 @@
-// Coming Soon. This file is the page of the accounts tracker feature, which will allow users to track their accounts and balances. It will include components for displaying account summaries, adding transactions, and visualizing account activity.
-
 import { Box, Heading, Center } from '@chakra-ui/react'
 import AccountsTracker from '../features/accounts/AccountsTracker'
-
 
 function AccountsTrackerPage() {
 
   return (
-    <Box bg="gray.200" p={4} minH="100vh">
-      <Box p={4} maxW="800px" mx="auto" borderWidth={1} borderRadius="lg" boxShadow="md" background={"white"}>
-        {/* <pre>{JSON.stringify(income, null, 2)}</pre>
-        <pre>{JSON.stringify(expenses, null, 2)}</pre> */}
+    <Box bg="gray.200" p={3} minH='96vh'>
+      <Box p={4} maxW="930px" mx="auto" borderWidth={1} borderRadius="lg" boxShadow="md" background={"white"}>
         <Center mb={4}>
-          <Heading>Accounts Tracker</Heading>
+          <Heading size="md" fontWeight={700}>Accounts Tracker</Heading>
         </Center>
         <AccountsTracker />
       </Box>
--- src/pages/BudgetTrackerPage.jsx
+++ src/pages/BudgetTrackerPage.jsx
@@ -11,8 +11,8 @@ import SavingsGoalsTracker from '../features/tracker/SavingsGoalsTracker';
 export default function BudgetTrackerPage() {
 
   return (
-    <Box bg="gray.200" p={4} minH="100vh">
-      <Box p={4} maxW="800px" mx="auto" borderWidth={1} borderRadius="lg" boxShadow="md" background={"white"}>
+    <Box bg="gray.200" py={4} minH='100vh'>
+      <Box p={4} maxW="800px" mx="auto" mb={'5vh'} borderWidth={1} borderRadius="lg" boxShadow="md" background={"white"}>
         <Center mb={4}>
           <Heading size="md" fontWeight={700}>Budget Tracker</Heading>
         </Center>
